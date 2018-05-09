24e42149c31bd7b25f080df8b274e4e35d305b49
kaekasui <kaekasui_github@icloud.com>
2018-05-09 21:35:52 +0900

    Add omniauth request spec

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -5,4 +5,19 @@ module RequestSpecHelper
     { Authorization: ActionController::HttpAuthentication::Token\
       .encode_credentials(user.access_token) }
   end
+
+  def create_omniauth_mock(service)
+    OmniAuth.config.test_mode = true
+
+    OmniAuth.config.mock_auth[service] = OmniAuth::AuthHash.new(
+      provider: service,
+      uid: '1234'
+    )
+
+    OmniAuth.config.add_mock(
+      service, info: { nickname: 'ニックネーム' }
+    )
+
+    OmniAuth.config.mock_auth[service]
+  end
 end
